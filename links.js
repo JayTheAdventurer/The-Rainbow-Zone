409b1dedb4eb2dd8bef55fe3311866495157ca44
JayTheAdventurer <jaytheadventurer1@gmail.com>
2025-02-25 10:47:44 -0500

    Organized files and created temporary landing page
    Organized all pages into it's own folder, there will be many more pages to come because of the gallery that needs to be made. Updated all pages to use the new javascript menus, and a failsafe just in case the user has javascript disabled.  Updated the sitemap to reflect new changes and site structure.

---
+++ links.js
@@ -0,0 +1,76 @@
+document.getElementById('headerlinks').innerHTML = 
+` <!--Header Starts-->
+    <div id="header">  
+        <div id="title"><a href=""></a></div><!--Title-->
+        <nav>
+            <div class="topmenu"> <!--Main Navigation-->
+                <img src="/The-Rainbow-Web/Assets/UI-UX/sitetitle.png" alt="">
+                
+                <a href="/The-Rainbow-Web/Pages/home.html">Home</a>
+
+                <a href="">Wtf is this?</a>
+
+                <a href="/The-Rainbow-Web/Pages/creations.html">Creations</a>
+
+                <a href="">Jay's Commission Zone</a>
+
+                <a href="/The-Rainbow-Web/Pages/thecrew.html">Meet the Crew</a>
+
+                <a href="/The-Rainbow-Web/Pages/journal.html">Journal</a>
+
+                <a href="">Link Page</a>
+
+                <a href="">Enter</a>
+            </div>
+        </nav><!--Main Navigation Ends-->
+    </div>
+    <!--Header Ends-->`;
+
+
+    document.getElementById('footerlink_button').innerHTML = 
+    `<div id="footer"> <!--Main Footer starts-->
+            <footer>
+                <a id="footertxt" href="#header">Back To Top</a><br>
+                <nav>
+                    <div id="bottommenu">
+                        <a href="https://jaytheadventurer.com/"><img src="/The-Rainbow-Web/Assets/Social Media Icons/jayy stuff hat sprite border shadow sm.png" alt="Jay The Adventurer Icon"></a>
+                        <a href="https://twitter.com/robotfluffy"><img src="/The-Rainbow-Web/Assets/Social Media Icons/Twitter icon test sm.gif" alt="Twitter Icon"></a>
+                        <a href="https://discord.com/invite/UdTBZF7"><img src="/The-Rainbow-Web/Assets/Social Media Icons/Discord icon test sm.gif" alt="Discord Icon"></a>
+                        <a href="https://www.deviantart.com/jayjaytheadventurer"><img src="/The-Rainbow-Web/Assets/Social Media Icons/DA Icon Test xm.gif" alt="DeviantArt Icon"></a>
+                        <a href="https://bsky.app/profile/jaytheadventurer.bsky.social"><img src="/The-Rainbow-Web/Assets/Social Media Icons/bluesky icon test sm.gif" alt=""></a>
+                        <a href="https://linktr.ee/jaytheadventurer"><img src="/The-Rainbow-Web/Assets/Social Media Icons/linktree icon test sm.gif" alt="Linktree icon"></a>
+                        <a href="https://yipstik-studios.itch.io/"><img src="/The-Rainbow-Web/Assets/Social Media Icons/yipstik icon- linktree xm.png" alt="Yipstick Icon"></a>
+                    </div>
+                </nav>
+
+                <div id="footerlinks">
+                    <a href="sitemap.html">Sitemap</a>
+                    <img src="/The-Rainbow-Web/Assets/UI-UX/dot.png" alt="Dot">
+                    <a href="">Privacy</a>
+                    <img src="/The-Rainbow-Web/Assets/UI-UX/dot.png" alt="Dot">
+                    <a href="">Terms Of Service</a>
+                </div>
+                <p id="footertxt">Created by Rainbow Parade in Febuary 2025</p>
+
+                
+            </footer>
+            <!--Footer Ends-->
+
+            <!-- buttons, blinkies, stamps, etc -->
+            <div class="button">
+                <img src="/The-Rainbow-Web/Assets/Blinkie/plural hell yea.gif" alt="Plural? Hell Yeah!">
+                <img src="/The-Rainbow-Web/Assets/Blinkie/plural symbol rainbow.gif" alt="Rainbow Plural Treblesand and Rings">
+                <img src="/The-Rainbow-Web/Assets/button wave/marcus pride button.gif" alt="">
+                <img src="/The-Rainbow-Web/Assets/button wave/plural flag wave const.gif" alt="Plural Treblesand Waving Flag">
+                <img src="/The-Rainbow-Web/Assets/button wave/pride button wave.gif" alt="Pride Waving Flag">
+            </div>
+            <div class="blinkie">
+                <img src="/The-Rainbow-Web/Assets/Blinkie/pride hell yeah.gif" alt="Pride? Hell Yeah">
+                <img src="/The-Rainbow-Web/Assets/Blinkie/pride hell yeah dark.gif" alt="Pride? Hell Yeah">
+                <img src="/The-Rainbow-Web/Assets/Blinkie/rainbow lover.gif" alt="">
+                <img src="/The-Rainbow-Web/Assets/Blinkie/taste the rainbow marcus blinkie.gif" alt="">
+            </div>
+            
+            
+        </div><!--Main Footer ends-->
+`;
