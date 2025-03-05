acfd4d9650711d22991ae8f78e520323f691ea21
JayTheAdventurer <jaytheadventurer1@gmail.com>
2025-03-04 23:30:29 -0500

    Added in accessibilities: pause gifs, change themes
    There will now be an accessibility menu that will be available for all pages. the home page is the one updated today. All gifs will be able to be paused, and now users can choose between a dark theme and a light theme.

--- links.js
+++ links.js
@@ -4,23 +4,23 @@ document.getElementById('headerlinks').innerHTML =
         <div id="title"><a href=""></a></div><!--Title-->
         <nav>
             <div class="topmenu"> <!--Main Navigation-->
-                <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/UI-UX/sitetitle.png" alt="">
+                <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/UI-UX/sitetitle.png" alt="The rainbow zone img title">
                 
-                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/home.html">Home</a>
+                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/home.html" alt="Homepage">Home</a>
 
-                <a href="">Wtf is this?</a>
+                <a href="" alt="About">Wtf is this?</a>
 
-                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/creations.html">Creations</a>
+                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/creations.html" alt="Art gallery and creative interests">Creations</a>
 
-                <a href="">Jay's Commission Zone</a>
+                <a href="" alt="Commissions (leads to outside site)">Jay's Commission Zone</a>
 
-                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/thecrew.html">Meet the Crew</a>
+                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/thecrew.html" alt="Introductions">Meet the Crew</a>
 
-                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/journal.html">Journal</a>
+                <a href="https://jaytheadventurer.github.io/The-Rainbow-Zone/Pages/journal.html" alt="Journal and Updates">Journal</a>
 
-                <a href="">Link Page</a>
+                <a href="" alt="Links, buttons, and our friends' sites">Link Page</a>
 
-                <a href="">Enter</a>
+                <a href="" alt="The rainbow zone entrance link">Enter</a>
             </div>
         </nav><!--Main Navigation Ends-->
     </div>
@@ -32,13 +32,13 @@ document.getElementById('headerlinks').innerHTML =
             <footer>
                 <a id="footertxt" href="#header">Back To Top</a><br>
                 <nav>
-                    <div id="bottommenu">
+                    <div id="bottommenu" >
                         <a href="https://jaytheadventurer.com/"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/jayy stuff hat sprite border shadow sm.png" alt="Jay The Adventurer Icon"></a>
-                        <a href="https://twitter.com/robotfluffy"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/Twitter icon test sm.gif" alt="Twitter Icon"></a>
-                        <a href="https://discord.com/invite/UdTBZF7"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/Discord icon test sm.gif" alt="Discord Icon"></a>
-                        <a href="https://www.deviantart.com/jayjaytheadventurer"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/DA Icon Test xm.gif" alt="DeviantArt Icon"></a>
-                        <a href="https://bsky.app/profile/jaytheadventurer.bsky.social"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/bluesky icon test sm.gif" alt=""></a>
-                        <a href="https://linktr.ee/jaytheadventurer"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/linktree icon test sm.gif" alt="Linktree icon"></a>
+                        <a class="freeze" href="https://twitter.com/robotfluffy"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/Twitter icon test sm.gif" alt="Twitter Icon"></a>
+                        <a class="freeze" href="https://discord.com/invite/UdTBZF7"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/Discord icon test sm.gif" alt="Discord Icon"></a>
+                        <a class="freeze" href="https://www.deviantart.com/jayjaytheadventurer"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/DA Icon Test xm.gif" alt="DeviantArt Icon"></a>
+                        <a class="freeze" href="https://bsky.app/profile/jaytheadventurer.bsky.social"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/bluesky icon test sm.gif" alt="bluesky icon"></a>
+                        <a class="freeze" href="https://linktr.ee/jaytheadventurer"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/linktree icon test sm.gif" alt="Linktree icon"></a>
                         <a href="https://yipstik-studios.itch.io/"><img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Social Media Icons/yipstik icon- linktree xm.png" alt="Yipstick Icon"></a>
                     </div>
                 </nav>
@@ -57,7 +57,7 @@ document.getElementById('headerlinks').innerHTML =
             <!--Footer Ends-->
 
             <!-- buttons, blinkies, stamps, etc -->
-            <div class="button">
+            <div class="button freeze">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Blinkie/plural hell yea.gif" alt="Plural? Hell Yeah!">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Blinkie/plural symbol rainbow.gif" alt="Rainbow Plural Treblesand and Rings">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/button wave/marcus pride button.gif" alt="">
@@ -65,7 +65,7 @@ document.getElementById('headerlinks').innerHTML =
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/button wave/pride button wave.gif" alt="Pride Waving Flag">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Rainbow/rainbow spiral button.gif" alt="Pride Waving Flag">
             </div>
-            <div class="blinkie">
+            <div class="blinkie freeze">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Blinkie/pride hell yeah.gif" alt="Pride? Hell Yeah">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Blinkie/pride hell yeah dark.gif" alt="Pride? Hell Yeah">
                 <img src="https://jaytheadventurer.github.io/The-Rainbow-Zone/Assets/Blinkie/rainbow lover.gif" alt="">
