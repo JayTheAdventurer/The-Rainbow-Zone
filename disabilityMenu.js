7143b13f7e1381ae6a557cadc5a09e4874b67409
JayTheAdventurer <jaytheadventurer1@gmail.com>
2025-03-05 22:19:49 -0500

    Update disabilityMenu.js
    updated the theme so hopefully it won't auto change to light theme when actually on dark mode and vice-versa

--- disabilityMenu.js
+++ disabilityMenu.js
@@ -5,9 +5,9 @@ function setTheme(themeName) {
 }// function to toggle between light and dark theme
 function toggleTheme() {
    if (localStorage.getItem('theme') === 'theme-dark'){
-       setTheme('theme-light');
-   } else {
        setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
    }
 }// Immediately invoked function to set the theme on initial load
 (function () {
