2114b1d356c07acb1293be0264d66286239cff4c
JayTheAdventurer <jaytheadventurer1@gmail.com>
2025-03-18 10:35:40 -0400

    Updated the image preview, and the styling for it
    Updated the slideshow to a preview, and changed the comment widget to use HMS time stamps. Made the preview pause gifs on button press as well

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,5 @@
+//slideshow code
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +28,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
+
+//image gallery code
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+  } 
